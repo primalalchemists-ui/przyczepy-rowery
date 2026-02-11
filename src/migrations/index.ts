7f5a02ebafa15f388cf2537e7499221d38f206f6
Mikołaj <ostermi@wp.pl>
2026-02-11 17:17:25 +0000

    migrations 2

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20260211_170046 from './20260211_170046';
+import * as migration_20260211_171629 from './20260211_171629';
 
 export const migrations = [
   {
-    up: migration_20260211_170046.up,
-    down: migration_20260211_170046.down,
-    name: '20260211_170046'
+    up: migration_20260211_171629.up,
+    down: migration_20260211_171629.down,
+    name: '20260211_171629'
   },
 ];
